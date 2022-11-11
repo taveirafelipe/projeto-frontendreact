a302f077f4e213acfaa18d97d7be658bad348712
taveirafelipe <taveira.felipe@gmail.com>
2022-11-11 17:18:09 -0300

    Pagging

---
+++ src/Components/Pagination/style.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Ul = styled.ul`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    // color: blue;
+    padding: 10px;
+`
+export const Li = styled.li`
+    list-style-type: none;    
+    padding: 5px;
+`
+export const Button = styled.button`
+    width: 20px;
+`
